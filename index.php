389eb81f4fb667a4eb3c18f1cf57ad46b84baff4
Aakarsh Chopra <alcatrazz.627@gmail.com>
2017-09-19 18:00:13 +0530

    Init

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php require_once('components/header.php'); ?>
+
+<div class="container-fluid text-center intro">
+    <h2>Welcome to the Events Repository of </h2>
+    <h1>Alumni Relations Cell</h1>
+    <h3>International &amp; Alumni Relations Student Council<br> IIT Madras</h3>
+</div>
+<div class="scroller"></div>
+<div class="scroller"></div>
+<?php require_once('components/footer.php'); ?>
